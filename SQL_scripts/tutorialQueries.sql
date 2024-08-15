930b156decd1cecb27e09e8fdb706a2a676b64f9
mwood <mwooden13@gmail.com>
2024-08-15 09:30:32 -0700

    distinct clause

--- SQL_scripts/tutorialQueries.sql
+++ SQL_scripts/tutorialQueries.sql
@@ -3,3 +3,8 @@ SELECT * FROM customer;
 
 -- Select specific columns from the customer table
 SELECT CUSTOMER_NAME, CITY, BALANCE FROM customer;
+
+--  select w/ DISTINCT Clause
+SELECT DISTINCT CITY FROM customer;
+-- select uniqure cities from the customer table
+
