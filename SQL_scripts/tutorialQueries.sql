1c6bf0d538cf62b95994f642ed4f67e45c207b24
mwood <mwooden13@gmail.com>
2024-08-15 09:37:37 -0700

    added scripts from tutorial

--- SQL_scripts/tutorialQueries.sql
+++ SQL_scripts/tutorialQueries.sql
@@ -4,7 +4,11 @@ SELECT * FROM customer;
 -- Select specific columns from the customer table
 SELECT CUSTOMER_NAME, CITY, BALANCE FROM customer;
 
---  select w/ DISTINCT Clause
+--  select w/ DISTINCT Clause select uniqure cities from the customer table
 SELECT DISTINCT CITY FROM customer;
--- select uniqure cities from the customer table
 
+-- return the customers in the city of fullton
+SELECT CUSTOMER_NAME, CITY, BALANCE FROM customer WHERE CITY = 'Fullton';
+
+-- add the <> 'not' character return queries not in fullton
+SELECT CUSTOMER_NAME, CITY, BALANCE FROM customer WHERE CITY <> 'Fullton';
