5ade2c7eb68df52d622475b1f24cb22f323e603f
mwood <mwooden13@gmail.com>
2024-09-04 23:54:10 -0700

    finish q5 for assignment two tables created

--- assignments/q_5.sql
+++ assignments/q_5.sql
@@ -2,25 +2,36 @@
 --student id, phone_num, phone_purpose(
 -- purpose is either cell, work, or home
 
-drop phone_purpose_table
-create phone_purpose_table ( 
-phone_type char(4));
+drop table phone_purpose_table;
+create table phone_purpose_table ( 
+phone_type char(4) primary key);
 
 -- Insert allowed phone purposes into phone_purpose_table
 insert into phone_purpose_table (phone_type) values ('cell');
 insert into phone_purpose_table (phone_type) values ('work');
 insert into phone_purpose_table (phone_type) values ('home');
 
-drop phone_table
-
+drop table phone_table;
+-- add phone table
 create table phone_table (
-student_id number primary key,
-phone varchar2(10) not null,
-phone_purpose char(4), 
-foreign key (phone_purpose) references phone_purpose_table(phone_type),
-foreign key (student_id) references student(student_id));
+    student_id number,
+    phone varchar2(10) not null,
+    phone_purpose char(4) not null, 
+    primary key (student_id, phone_purpose),
+    foreign key (phone_purpose) references phone_purpose_table(phone_type),
+    foreign key (student_id) references student(student_id)
+);
+
+-- insert new entry for mary and her phone
+insert into phone_table (student_id, phone, phone_purpose)
+values ((select student_id from student where name = 'Mary'),'1234567890','cell');
 
 insert into phone_table (student_id, phone, phone_purpose)
-values (
-    (select student_id from student where name = 'Mary'), 
-    '1234567890', 
+values ((select student_id from student where name = 'Mary'),'0987654321','work');
+
+select * from phone_table;
+
+insert into phone_table (student_id, phone, phone_purpose)
+values ((select student_id from student where name = 'Mary'),'0987654321','mobl');
+
+select * from phone_table;
