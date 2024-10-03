1cd4231dd27f81c916e9813a5e6ca66dcfe1b779
mwood <mwooden13@gmail.com>
2024-10-03 00:11:05 -0700

    added view sql

---
+++ assignments/table-manipulations/views.sql
@@ -0,0 +1,12 @@
+-- Create a view to show student nicknames, phone numbers, and phone types
+CREATE OR REPLACE VIEW student_nickname_phone AS
+SELECT sr.STUDENT_NAME AS nickname, sr.Phone_Number, pp.PHONE_TYPE
+FROM student_roster sr
+JOIN phone_purpose pp ON sr.Phone_Number = pp.PHONE_TYPE;
+
+-- Query the view
+SELECT * FROM student_nickname_phone;
+
+-- Optionally, drop the view
+-- DROP VIEW student_nickname_phone;
+
